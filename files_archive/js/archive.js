53cf6f4b8b8c00f27b29c18c3fe6d14f30f48454
Vincent Petry <pvince81@owncloud.com>
2013-10-18 15:23:28 +0200

    WIP: start viewer call

--- files_archive/js/archive.js
+++ files_archive/js/archive.js
@@ -9,19 +9,19 @@ $(document).ready(function() {
 	if(OC.currentUser) {
 		if(typeof FileActions!=='undefined'){
 			FileActions.register('application/zip','Open', OC.PERMISSION_READ, '',function(filename){
-				window.location=OC.linkTo('files', 'index.php')+'?dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+				FileList.changeDirectory($('#dir').val() + '/' + filename);
 			});
 			FileActions.setDefault('application/zip','Open');
 			FileActions.register('application/x-gzip','Open', OC.PERMISSION_READ, '',function(filename){
-				window.location=OC.linkTo('files', 'index.php')+'?dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+				FileList.changeDirectory($('#dir').val() + '/' + filename);
 			});
 			FileActions.setDefault('application/x-compressed','Open');
 			FileActions.register('application/x-compressed','Open', OC.PERMISSION_READ, '',function(filename){
-				window.location=OC.linkTo('files', 'index.php')+'?dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+				FileList.changeDirectory($('#dir').val() + '/' + filename);
 			});
 			FileActions.setDefault('application/x-compressed','Open');
 			FileActions.register('application/x-tar','Open', OC.PERMISSION_READ, '',function(filename){
-				window.location=OC.linkTo('files', 'index.php')+'?dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+				FileList.changeDirectory($('#dir').val() + '/' + filename);
 			});
 			FileActions.setDefault('application/x-tar','Open');
 		}
